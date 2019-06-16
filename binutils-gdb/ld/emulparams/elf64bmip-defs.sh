6f02a52ccfb9dec3538f5e3419e427327eefd476
ty q <qiaotian@umich.edu>
2019-06-16 12:44:56 +0800

    riscv support

---
+++ binutils-gdb/ld/emulparams/elf64bmip-defs.sh
@@ -0,0 +1,6 @@
+source_sh ${srcdir}/emulparams/elf32bmipn32-defs.sh
+COMMONPAGESIZE="CONSTANT (COMMONPAGESIZE)"
+INITIAL_READONLY_SECTIONS="
+  .MIPS.abiflags      ${RELOCATING-0} : { *(.MIPS.abiflags) }
+  .MIPS.options : { *(.MIPS.options) }
+"
